46a0f3a089c14e7aa94e3a3e065254fd9c3ef015
Andrew Wright <a.wright@kainos.com>
2014-04-23 08:03:31 +0100

    adding spotify control aliases

--- spotify/alias.zsh
+++ spotify/alias.zsh
@@ -1,1 +1,5 @@
 alias s="osascript ~/.dotfiles/spotify/controller.scpt"
+
+alias sp="osascript ~/.dotfiles/spotify/controller.scpt play";
+alias ss="osascript ~/.dotfiles/spotify/controller.scpt stop";
+alias sn="osascript ~/.dotfiles/spotify/controller.scpt next";
