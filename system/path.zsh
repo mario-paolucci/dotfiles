803726f269024c5b2e41a44f82386ac73ec9d9fe
Andrew Wright <a.wright@kainos.com>
2014-06-20 09:48:27 +0100

    Added oracle environment variable and changes window title stuff

--- system/path.zsh
+++ system/path.zsh
@@ -5,4 +5,5 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 source /opt/boxen/env.sh
 
 export PATH=$PATH:/Users/andrewwr/work/vag_vm/OracleInstantClient
+export DYLD_LIBRARY_PATH=/Users/andrewwr/work/vag_vm/OracleInstantClient
 export TNS_ADMIN=/Users/andrewwr/work/vag_vm/OracleInstantClient
