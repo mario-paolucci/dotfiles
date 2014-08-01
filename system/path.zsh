aaf983b5e4a98b60180d257e32237f1114a6eeed
Andrew Wright <a.wright@kainos.com>
2014-08-01 18:16:35 +0100

    Add pathogen to vim, add java home, add other global variables

--- system/path.zsh
+++ system/path.zsh
@@ -5,5 +5,7 @@ export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 source /opt/boxen/env.sh
 
 export PATH=$PATH:/Users/andrewwr/work/vag_vm/OracleInstantClient
+export PATH=$PATH:/Users/andrewwr/work/capd-project/usefulScripts
 export DYLD_LIBRARY_PATH=/Users/andrewwr/work/vag_vm/OracleInstantClient
 export TNS_ADMIN=/Users/andrewwr/work/vag_vm/OracleInstantClient
+
