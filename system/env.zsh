425e59c1b1b2a31dd07c90d1e59ae901be3fdad4
Mario Paolucci <mario@kainos.com>
2016-12-01 23:17:51 +0000

    fixes

--- system/env.zsh
+++ system/env.zsh
@@ -3,3 +3,9 @@ if [[ "$EDITOR" == "" ]] ; then
   # Use sublime for my editor.
   export EDITOR='emacs'
 fi
+
+export dvsa_workspace="/Users/mariopaolucci/MOTDEV"
+export dev_workspace="${dvsa_workspace}/mot"
+export scripts_workspace="${dvsa_workspace}/mot-vagrant"
+export WORKSPACE="${dvsa_workspace}/mot"
+export puppet_workspace="${dvsa_workspace}/puppet-code"
--- system/path.zsh
+++ system/path.zsh
@@ -1,11 +1,3 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="${HOME}/MOTDEV/devkit/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
-
-source /opt/boxen/env.sh
-
-export PATH=$PATH:/Users/andrewwr/work/vag_vm/OracleInstantClient
-export PATH=$PATH:/Users/andrewwr/work/capd-project/usefulScripts
-export DYLD_LIBRARY_PATH=/Users/andrewwr/work/vag_vm/OracleInstantClient
-export TNS_ADMIN=/Users/andrewwr/work/vag_vm/OracleInstantClient
-
